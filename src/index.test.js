814324473b5caf0d55e5a1f0e48ba03766b2014d
Geoff Hollingworth <eusholli@gmail.com>
2020-02-19 14:57:35 -0600

    ci(package.json): removed vulnerability

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
+var assert = require('assert');
 var expect = require('chai').expect;
-var mobiledgeX = require('./index.js');
+var should = require('chai').should();
 
-describe('mobiledgeXClient', function () {
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised).should();
+
+var mobiledgeXClient = require('./index.js').MobiledgeXClient;
+
+const devName = "MobiledgeX"; // Your developer name
+const appName = "MobiledgeX SDK Demo"; // Your application name
+const appVersionStr = "2.0";
+client = new mobiledgeXClient(devName, appName, appVersionStr);
+
+describe('mobiledgeXClient registerclient', function () {
+    it('should return true if valid user id', function () {
+        return loginController.isAuthorizedPromise('abc123').should.eventually.be.true;
+    });
     it('should work!', function () {
         expect(true).to.be.true;
     })
